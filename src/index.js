185f020f43fb132a540fc64204e4fcb19666c8fc
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-06-01 23:08:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 //------------------------------------------------------
-
 
 const API_KEY = '36885603-fb02061b93c5e3b035d34c370';
 const BASE_URL = 'https://pixabay.com/api/';
@@ -13,6 +12,18 @@ const refs = {
     inputSearch: document.querySelector('[name="searchQuery"]'),
     listCard: document.querySelector('.js-list-cards')
 }
+
+const guard = document.querySelector('.js-guard');
+//--* INTRSECTION OBSERVER *----------------------------------------------------
+
+let options = {
+    root: null,
+    rootMargin: "330px",
+    threshold: 0,
+}
+
+let observer = new IntersectionObserver(handlerPagination, options);
+
 let page = 1;
 //events on btn
 refs.form.addEventListener('submit', hendlerSearchForm);
@@ -33,7 +44,7 @@ async function hendlerSearchForm(event) {
 /**========================================================================
  * function for create request on server and get data
  */
-async function serviceSearchImages(page = 1) {
+async function serviceSearchImages() {
 
     const inputValue = refs.inputSearch.value.trim();
     // console.log(inputValue)
@@ -41,13 +52,14 @@ async function serviceSearchImages(page = 1) {
     const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(inputValue)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
     // console.log(inputValue)
 
-    await axios.get(url)
-        .then(response => {
-            const { data } = response;
-            const imagesAll = data.hits;
-            const totalHits = data.totalHits;
+    try {
+        const response = await axios.get(url);
 
-            if (imagesAll.length === 0) {
+        const { data } = response;
+        const imagesAll = data.hits;
+        const totalHits = data.totalHits;
+
+        if (imagesAll.length === 0) {
                 if (page === 1) {
                     Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again!')
                 }
@@ -61,18 +73,43 @@ async function serviceSearchImages(page = 1) {
                 // Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
                 observer.unobserve(guard);
             }
+    } catch (error) {
+        console.log(error)
+    }
+    
 
-        })
-        .catch(error => {
-            console.log(error)
-        });
+    // await axios.get(url)
+    //     .then(response => {
+    //         const { data } = response;
+    //         const imagesAll = data.hits;
+    //         const totalHits = data.totalHits;
+
+    //         if (imagesAll.length === 0) {
+    //             if (page === 1) {
+    //                 Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again!')
+    //             }
+    //         } else {
+    //             Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`)
+    //             renderDataImage(imagesAll);
+    //         }
+    //         if (imagesAll.length < totalHits) {
+    //             observer.observe(guard);
+    //         } else if (imagesAll.length === totalHits) {
+    //             // Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
+    //             observer.unobserve(guard);
+    //         }
+
+    //     })
+    //     .catch(error => {
+    //         console.log(error)
+    //     });
 }
 
 /**========================================================================
  * function for create  markup 
  * @param {*object} image 
  */
-function createMarkup(image, lightbox) {
+function createMarkup(image) {
     return `
     <div class="photo-card">
         <div class="gallery">
@@ -97,12 +134,13 @@ function renderDataImage(array) {
     // refs.listCard.innerHTML = cardsImage;
     refs.listCard.insertAdjacentHTML('beforeend', cardsImage);
 
-    //------- usage simplelightbox -----------------------------------
-    const lightbox = new SimpleLightbox('.gallery a');
-    //----------------* Smooth page scrolling *-----------------------
+    // //------- usage simplelightbox -----------------------------------
+    // const lightbox = new SimpleLightbox('.gallery a');
+
+    //----------------* Smooth page scrolling *--------------------------
     const { height: cardHeight } = refs.listCard.firstElementChild.getBoundingClientRect();
     window.scrollBy({
-        top: cardHeight * 0,
+        top: cardHeight * 2,
         behavior: "smooth",
 });
 }
@@ -112,31 +150,17 @@ function renderDataImage(array) {
  */
 function clearListCards() {
     refs.listCard.innerHTML = '';
-    // refs.inputSearch.value = '';
 }
 
-
-//--* INTRSECTION OBSERVER *----------------------------------------------------
-
-const guard = document.querySelector('.js-guard');
-
-let options = {
-    root: null,
-    rootMargin: "330px",
-    threshold: 0,
-}
-
-let observer = new IntersectionObserver(handlerPagination, options);
-
-/**
+/**===========================================================================
  * function callback Intersection observer
- * @param {*} entries 
+ * @param {*params} entries //It is params of global class IntersectionObserver 
  */
-function handlerPagination(entries) {
-    entries.forEach(entry => {
+async function handlerPagination(entries) {
+    entries.forEach(async entry => {
         if (entry.isIntersecting) {
             page += 1;
-            serviceSearchImages(page);
+            await serviceSearchImages();
         }
     })
 }
